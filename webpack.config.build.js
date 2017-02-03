e5aec8d596a396bd046bab6c3467c9d784f77ade
git <wtm19951028>
2017-02-03 13:15:30 +0800

    修复build文件

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -6,7 +6,6 @@ let path = require('path');		//引入node的path库
 let HtmlWebpackPlugin = require('html-webpack-plugin');
 //压缩代码
 let UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-let LoaderOptionsPlugin = webpack.LoaderOptionsPlugin;
 let autoprefixer = require('autoprefixer')
 
 // 获取所有的.css文件，合并它们的内容然后提取css内容到一个独立的”styles.css“
@@ -28,7 +27,7 @@ let config = {
 			//为WebPack指定loaders
 			{
 				test: /\.less$/,
-				loader: ExtractTextPlugin.extract(["style-loader", "css-loader!less-loader!postcss-loader"]),
+				loader: ExtractTextPlugin.extract(("style-loader", "css-loader!less-loader")),
 				include: path.resolve(__dirname, 'app')
 			},
 			{
@@ -39,12 +38,12 @@ let config = {
 					presets: ['react', 'es2015']
 				}
 			},
-			{
-				// I want to uglify with mangling only app files, not thirdparty libs
-				test: /.*\/app\/.*\.js$/,
-				exclude: /.spec.js/, // excluding .spec files
-				loader: "uglify"
-			},
+			// {
+			// 	// I want to uglify with mangling only app files, not thirdparty libs
+			// 	test: /.*\/app\/.*\.js$/,
+			// 	exclude: /.spec.js/, // excluding .spec files
+			// 	loader: "uglify"
+			// },
 			{
 				test: /.(png|jpg|jpeg|gif|svg|webp)$/, 
 				loader: "url-loader?limit=30720"
@@ -63,7 +62,7 @@ let config = {
 				NODE_ENV: JSON.stringify('production')
 			}
 		}),
-		// new UglifyJsPlugin({minmize: true}),
+		new UglifyJsPlugin({minmize: true}),
 		// new UglifyJsPlugin(),
 	],
 };
